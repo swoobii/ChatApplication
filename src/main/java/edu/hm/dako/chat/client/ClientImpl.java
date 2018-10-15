98b8ce1defc82ab44fff1656156bb47d88169579
Sebastian Swoboda <swoboda@hm.edu>
2018-10-15 18:20:06 +0200

    Chat App

---
+++ src/main/java/edu/hm/dako/chat/client/ClientImpl.java
@@ -0,0 +1,45 @@
+package edu.hm.dako.chat.client;
+
+import edu.hm.dako.chat.common.ExceptionHandler;
+import edu.hm.dako.chat.common.SystemConstants;
+
+/**
+ * <p/>
+ * Verwaltet eine Verbindung zum Server.
+ *
+ * @author Mandl
+ */
+public class ClientImpl extends AbstractChatClient {
+
+	/**
+	 * Konstruktor
+	 * 
+	 * @param userInterface
+	 *          Schnittstelle zum User-Interface
+	 * @param serverPort
+	 *          Portnummer des Servers
+	 * @param remoteServerAddress
+	 *          IP-Adresse/Hostname des Servers
+	 */
+
+	public ClientImpl(ClientUserInterface userInterface, int serverPort,
+			String remoteServerAddress, String serverType) {
+
+		super(userInterface, serverPort, remoteServerAddress);
+		this.serverPort = serverPort;
+		this.remoteServerAddress = remoteServerAddress;
+
+		Thread.currentThread().setName("Client");
+		threadName = Thread.currentThread().getName();
+
+		try {
+			// Simple TCP Server erzeugen
+			messageListenerThread = new SimpleMessageListenerThreadImpl(userInterface,
+						connection, sharedClientData);
+
+			messageListenerThread.start();
+		} catch (Exception e) {
+			ExceptionHandler.logException(e);
+		}
+	}
+}
